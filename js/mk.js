d0947d77319b0de3fbbb1746b04bdeac142bb297
Joseka <josekamt@alumnos.ilernasevilla.com>
2024-02-08 10:27:21 +0100

    casi terminado

--- js/mk.js
+++ js/mk.js
@@ -1,31 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const card = document.getElementById('interactiveCard');
-  card.style.transform = 'scale(0)';
-  setTimeout(() => {
-    card.style.transform = 'scale(1)';
-    card.style.transition = 'transform 0.5s ease';
-  }, 300);
-
-  const toggleDetailsBtn = document.getElementById('toggleDetails');
-  // Selecciona tanto párrafos como encabezados h5
-  const infoElements = document.querySelectorAll('.card-info p, .card-info h5');
-  let detailsVisible = false;
-
-  toggleDetailsBtn.addEventListener('click', function() {
-    detailsVisible = !detailsVisible;
-    infoElements.forEach((element, index) => {
-      if (detailsVisible) {
-        setTimeout(() => {
-          element.classList.add('active');
-        }, 100 * index); // Retraso incremental para cada elemento
-      } else {
-        element.classList.remove('active');
-      }
-    });
-    this.textContent = detailsVisible ? 'Ocultar Detalles' : 'Mostrar Detalles';
-  });
-
-
+  const cards = document.querySelectorAll('.interactiveCard');
   
-});
+  cards.forEach(card => {
+    card.style.transform = 'scale(0)';
+    setTimeout(() => {
+      card.style.transform = 'scale(1)';
+      card.style.transition = 'transform 0.5s ease';
+    }, 300);
 
+    const toggleDetailsBtn = card.querySelector('.toggleDetails');
+    const infoElements = card.querySelectorAll('.card-info p, .card-info h5');
+    let detailsVisible = false;
+
+    toggleDetailsBtn.addEventListener('click', function() {
+      detailsVisible = !detailsVisible;
+      infoElements.forEach((element, index) => {
+        if (detailsVisible) {
+          setTimeout(() => {
+            element.classList.add('active');
+          }, 100 * index); // Retraso incremental para cada elemento
+        } else {
+          element.classList.remove('active');
+        }
+      });
+      this.textContent = detailsVisible ? 'Ocultar Detalles' : 'Mostrar Detalles';
+    });
+  });
+});
