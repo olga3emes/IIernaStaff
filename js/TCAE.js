2c535aac3a6ea5206419ae9e4d0a37907dc71c60
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2024-02-19 10:23:02 +0100

    tcae

--- js/TCAE.js
+++ js/TCAE.js
@@ -1,5 +1,13 @@
 "use strict"
 
+// Ocultar todas las tarjetas al cargar la página
+document.addEventListener("DOMContentLoaded", function() {
+  var cards = document.querySelectorAll('.card-body');
+  cards.forEach(function(card) {
+      card.style.display = 'none';
+  });
+});
+
 function toggleCardBody(cardId) {
   // Ocultar todas las tarjetas
   var cards = document.querySelectorAll('.card-body');
